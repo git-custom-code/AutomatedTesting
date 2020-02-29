0e44db18d8c51c0ae67bf8cb5deafa39122aa266
Stefan Loeffler <s.loeffler@custom-co.de>
2020-02-29 08:50:38 +0100

    added strongly typed exceptions for better exception handling

---
+++ src/AutomatedTesting.Mocks/ExceptionHandling/MethodInfoException.cs
@@ -0,0 +1,35 @@
+namespace CustomCode.AutomatedTesting.Mocks.ExceptionHandling
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Exception that is throw whenever a <see cref="MethodInfo"/> could not be found via reflection.
+    /// </summary>
+    public sealed class MethodInfoException : ReflectionException
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MethodInfoException"/> type.
+        /// </summary>
+        /// <param name="sourceType"> The <see cref="Type"/> whose method could not be found. </param>
+        /// <param name="methodName"> The name of the method that could not be found. </param>
+        public MethodInfoException(Type sourceType, string methodName)
+            : base($"Unable to find method '{sourceType.Name}.{methodName}' via reflection", sourceType)
+        {
+            MethodName = methodName;
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// Gets the name of the method that could not be found.
+        /// </summary>
+        public string MethodName { get; }
+
+        #endregion
+    }
+}
---
+++ src/AutomatedTesting.Mocks/ExceptionHandling/ReflectionException.cs
@@ -0,0 +1,34 @@
+namespace CustomCode.AutomatedTesting.Mocks.ExceptionHandling
+{
+    using System;
+
+    /// <summary>
+    /// Base type for exceptions that are caused by reflection.
+    /// </summary>
+    public abstract class ReflectionException : Exception
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ReflectionException"/> type.
+        /// </summary>
+        /// <param name="message"> The message that describes the error that has occured. </param>
+        /// <param name="sourceType"> The <see cref="Type"/> that has caused the exception to be thrown. </param>
+        protected ReflectionException(string message, Type sourceType)
+            : base(message)
+        {
+            SourceType = sourceType;
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// Gets the <see cref="Type"/> that has caused the reflection to be thrown.
+        /// </summary>
+        public Type SourceType { get; }
+
+        #endregion
+    }
+}
