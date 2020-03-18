d662283e02da2b1102465f523779ff6d4cff3c0b
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-18 21:50:46 +0100

    refactoring: added dynamic code for IPropertySetterValue invocation feature

--- src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Features.Property.cs
+++ src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Features.Property.cs
@@ -1,7 +1,6 @@
 namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
 {
     using ExceptionHandling;
-    using Interception.Parameters;
     using Interception.Properties;
     using System;
     using System.Reflection;
@@ -19,6 +18,12 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
         /// </summary>
         private static Lazy<ConstructorInfo> CreatePropertyInvocation { get; }
             = new Lazy<ConstructorInfo>(InitializeCreatePropertyInvocation, true);
+
+        /// <summary>
+        /// Gets the cached signature of the <see cref="PropertySetterValue"/> constructor.
+        /// </summary>
+        private static Lazy<ConstructorInfo> CreatePropertySetterValue { get; }
+            = new Lazy<ConstructorInfo>(InitializeCreatePropertySetterValue, true);
 
         #endregion
 
@@ -39,6 +44,25 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
             this ILGenerator body, out LocalBuilder propertyFeatureVariable)
         {
             propertyFeatureVariable = body.DeclareLocal(typeof(PropertyInvocation));
+        }
+
+        /// <summary>
+        /// Emits a local <see cref="PropertySetterValue"/> feature variable.
+        /// </summary>
+        /// <param name="body"> The body of the dynamic property setter. </param>
+        /// <param name="propertySetterValueFeatureVariable">
+        /// The emitted local <see cref="PropertySetterValue"/> variable.
+        /// </param>
+        /// <remarks>
+        /// Emits the following source code:
+        /// <![CDATA[
+        ///     PropertySetterValue propertySetterValueFeature;
+        /// ]]>
+        /// </remarks>
+        public static void EmitLocalPropertySetterValueFeatureVariable(
+            this ILGenerator body, out LocalBuilder propertySetterValueFeatureVariable)
+        {
+            propertySetterValueFeatureVariable = body.DeclareLocal(typeof(PropertySetterValue));
         }
 
         /// <summary>
@@ -67,6 +91,40 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
         }
 
         /// <summary>
+        /// Emits code to create a new <see cref="PropertySetterValue"/> instance for the given feature.
+        /// </summary>
+        /// <param name="body"> The body of the dynamic property setter. </param>
+        /// <param name="signature"> The property's signature. </param>
+        /// <param name="propertySignatureVariable"> The emitted local <see cref="PropertyInfo"/> variable. </param>
+        /// <param name="propertySetterValueFeatureVariable"> The emitted local <see cref="PropertySetterValue"/> variable. </param>
+        /// <remarks>
+        /// Emits the following source code:
+        /// <![CDATA[
+        ///     propertySetterValueFeature = new PropertySetterValue(propertySignature, value);
+        /// ]]>
+        /// </remarks>
+        public static void EmitNewPropertySetterValueFeature(
+            this ILGenerator body,
+            PropertyInfo signature,
+            LocalBuilder propertySignatureVariable,
+            LocalBuilder propertySetterValueFeatureVariable)
+        {
+            // propertySignature,
+            body.Emit(OpCodes.Ldloc, propertySignatureVariable.LocalIndex);
+
+            // value
+            body.Emit(OpCodes.Ldarg_1);
+            if (signature.PropertyType.IsValueType)
+            {
+                body.Emit(OpCodes.Box, signature.PropertyType);
+            }
+
+            // propertySetterValueFeature = new PropertySetterValue(...)
+            body.Emit(OpCodes.Newobj, CreatePropertySetterValue.Value);
+            body.Emit(OpCodes.Stloc, propertySetterValueFeatureVariable.LocalIndex);
+        }
+
+        /// <summary>
         /// Initialization logic for the <see cref="CreatePropertyInvocation"/> property.
         /// </summary>
         /// <returns> The signature of the <see cref="PropertyInvocation"/> constructor. </returns>
@@ -74,6 +132,17 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
         {
             var type = typeof(PropertyInvocation);
             var constructor = type.GetConstructor(new[] { typeof(PropertyInfo) });
+            return constructor ?? throw new ConstructorInfoException(type);
+        }
+
+        /// <summary>
+        /// Initialization logic for the <see cref="CreatePropertySetterValue"/> property.
+        /// </summary>
+        /// <returns> The signature of the <see cref="PropertySetterValue"/> constructor. </returns>
+        private static ConstructorInfo InitializeCreatePropertySetterValue()
+        {
+            var type = typeof(PropertySetterValue);
+            var constructor = type.GetConstructor(new[] { typeof(PropertyInfo), typeof(object) });
             return constructor ?? throw new ConstructorInfoException(type);
         }
 
