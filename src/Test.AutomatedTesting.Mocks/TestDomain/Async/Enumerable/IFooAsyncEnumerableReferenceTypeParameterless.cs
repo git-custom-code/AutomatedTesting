cd21266a5001597d37bb3a4035afa6c4500d9e89
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-20 13:08:53 +0100

    added unit tests for async (reference type) methods that return a IAsyncEnumerable<T>

---
+++ src/Test.AutomatedTesting.Mocks/TestDomain/Async/Enumerable/IFooAsyncEnumerableReferenceTypeParameterless.cs
@@ -0,0 +1,13 @@
+namespace CustomCode.AutomatedTesting.Mocks.TestDomain
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface that simulates a dependency that contains an asynchronous method
+    /// without parameters that returns an <see cref="IAsyncEnumerable{T}"/> with reference types.
+    /// </summary>
+    public interface IFooAsyncEnumerableReferenceTypeParameterless : IFoo
+    {
+        IAsyncEnumerable<object?> MethodWithoutParameterAsync();
+    }
+}
