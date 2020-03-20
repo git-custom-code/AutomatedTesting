69968d7af20f4c193fb34713bad68a75c424c116
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-20 12:53:12 +0100

    added unit tests for async methods that return a ValueTask

---
+++ src/Test.AutomatedTesting.Mocks/Emitter/MethodEmitters/InterceptAsyncMethodEmitterTests.ValueTask.cs
@@ -0,0 +1,41 @@
+namespace CustomCode.AutomatedTesting.Mocks.Emitter.Tests
+{
+    #region Usings
+
+    using Interception.Async;
+    using Mocks.Tests.Extensions;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using TestDomain;
+    using Xunit;
+
+    #endregion
+
+    /// <summary>
+    /// Automated tests for the <see cref="InterceptAsyncMethodEmitter{T}"/> type.
+    /// </summary>
+    public sealed partial class InterceptAsyncMethodEmitterTests
+    {
+        [Fact(DisplayName = "MethodEmitter: ValueTask without parameters")]
+        public async Task ValueTaskWithoutParametersAsync()
+        {
+            // Given
+            var proxyFactory = CreateFactory();
+            var interceptor = new AsyncInterceptor();
+
+            // When
+            var foo = proxyFactory.CreateForInterface<IFooValueTaskParameterless>(interceptor);
+            var task = foo.MethodWithoutParameterAsync();
+            await task.ConfigureAwait(false);
+
+            // Then
+            Assert.NotNull(foo);
+
+            Assert.Single(interceptor.ForwardedInvocations);
+            var invocation = interceptor.ForwardedInvocations.Single();
+            invocation.ShouldInterceptMethodWithName(nameof(IFooValueTaskParameterless.MethodWithoutParameterAsync));
+            invocation.ShouldBeAsyncInvocationOfType(AsyncInvocationType.ValueTask);
+            invocation.ShouldHaveNoParameterIn();
+        }
+    }
+}
---
+++ src/Test.AutomatedTesting.Mocks/TestDomain/Async/ValueTask/IFooValueTaskParameterless.cs
@@ -0,0 +1,13 @@
+namespace CustomCode.AutomatedTesting.Mocks.TestDomain
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface that simulates a dependency that contains an asynchronous method
+    /// without parameters that returns a <see cref="ValueTask"/>.
+    /// </summary>
+    public interface IFooValueTaskParameterless : IFoo
+    {
+        ValueTask MethodWithoutParameterAsync();
+    }
+}
