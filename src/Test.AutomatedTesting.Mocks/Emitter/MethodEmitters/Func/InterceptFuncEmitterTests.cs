0995009f52c1d41669a3375af6326de71f3cee29
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-22 15:29:36 +0100

    added unit test for InterceptFuncEmitter

---
+++ src/Test.AutomatedTesting.Mocks/Emitter/MethodEmitters/Func/InterceptFuncEmitterTests.cs
@@ -0,0 +1,131 @@
+namespace CustomCode.AutomatedTesting.Mocks.Emitter.Tests
+{
+    #region Usings
+
+    using Core.Context;
+    using Core.Extensions;
+    using CustomCode.AutomatedTesting.Mocks.Interception.ReturnValue;
+    using Interception;
+    using Interception.Parameters;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using TestDomain;
+    using Xunit;
+
+    #endregion
+
+    /// <summary>
+    /// Automated tests for the <see cref="InterceptFuncEmitter{T}"/> type.
+    /// </summary>
+    public sealed partial class InterceptFuncEmitterTests : IClassFixture<ProxyFactoryContext>
+    {
+        #region Dependencies
+
+        public InterceptFuncEmitterTests(ProxyFactoryContext context)
+        {
+            Context = context;
+        }
+
+        private ProxyFactoryContext Context { get; }
+
+        #endregion
+
+        #region Interceptor
+
+        private sealed class FuncInterceptor<T> : IInterceptor
+        {
+            public FuncInterceptor([AllowNull] T value)
+            {
+                Value = value;
+            }
+
+            [AllowNull, MaybeNull]
+            private T Value { get; }
+
+            public List<IInvocation> ForwardedInvocations { get; } = new List<IInvocation>();
+
+            public void Intercept(IInvocation invocation)
+            {
+                ForwardedInvocations.Add(invocation);
+                if (invocation.TryGetFeature<IReturnValue<T>>(out var feature))
+                {
+                    feature.ReturnValue = Value;
+                }
+            }
+        }
+
+        private sealed class ReplaceRefParameterInterceptor<T> : IInterceptor
+        {
+            public ReplaceRefParameterInterceptor([AllowNull] T value)
+            {
+                Value = value;
+            }
+
+            [AllowNull, MaybeNull]
+            private T Value { get; }
+
+            public List<IInvocation> ForwardedInvocations { get; } = new List<IInvocation>();
+
+            public void Intercept(IInvocation invocation)
+            {
+                ForwardedInvocations.Add(invocation);
+                if (invocation.TryGetFeature<IParameterRef>(out var parameterRef))
+                {
+                    foreach (var parameter in parameterRef.RefParameterCollection)
+                    {
+                        if (parameter.Type.IsValueType)
+                        {
+                            parameter.Value = Activator.CreateInstance(parameter.Type);
+                        }
+                        else
+                        {
+                            parameter.Value = null;
+                        }
+                    }
+                }
+
+                if (invocation.TryGetFeature<IReturnValue<T>>(out var feature))
+                {
+                    feature.ReturnValue = Value;
+                }
+            }
+        }
+
+        private sealed class OutParameterInterceptor<T> : IInterceptor
+        {
+            public OutParameterInterceptor([AllowNull] T result, [AllowNull] T value)
+            {
+                Result = result;
+                Value = value;
+            }
+
+            [AllowNull, MaybeNull]
+            private T Result { get; }
+
+            [AllowNull, MaybeNull]
+            private T Value { get; }
+
+            public List<IInvocation> ForwardedInvocations { get; } = new List<IInvocation>();
+
+            public void Intercept(IInvocation invocation)
+            {
+                ForwardedInvocations.Add(invocation);
+                if (invocation.TryGetFeature<IParameterOut>(out var parameterOut))
+                {
+                    foreach (var parameter in parameterOut.OutParameterCollection)
+                    {
+                        parameter.Value = Value;
+                    }
+                }
+                if (invocation.TryGetFeature<IReturnValue<T>>(out var feature))
+                {
+                    feature.ReturnValue = Result;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
