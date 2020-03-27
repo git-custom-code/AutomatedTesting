cfdc3eaaa0b26c003193b7131a52578f5f3f5655
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-27 12:55:03 +0100

    added dynamic code generation for calling through to a decoratee's method

---
+++ src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Decorator.cs
@@ -0,0 +1,45 @@
+namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
+{
+    using System.Reflection;
+    using System.Reflection.Emit;
+
+    /// <summary>
+    /// Extension methods for the <see cref="ILGenerator"/> type.
+    /// </summary>
+    public static partial class ILGeneratorExtensions
+    {
+        #region Logic
+
+        /// <summary>
+        /// </summary>
+        /// <param name="body"> The body of the dynamic method or property. </param>
+        /// <param name="signature"> The signature of the method to be called on the decorated instance. </param>
+        /// <param name="parameterSignatures"> The signatures of the method's parameters. </param>
+        /// <param name="decorateeField"> The backing field that holds the decorated instance. </param>
+        /// <remarks>
+        /// Emits the following source code:
+        ///
+        /// <![CDATA[
+        ///     _decorate.Method(parameter1, ... parameterN);
+        /// ]]>
+        /// </remarks>
+        public static void EmitDecorateeCall(
+            this ILGenerator body,
+            MethodInfo signature,
+            ParameterInfo[] parameterSignatures,
+            FieldBuilder decorateeField)
+        {
+            body.Emit(OpCodes.Ldarg_0);
+            body.Emit(OpCodes.Ldfld, decorateeField);
+
+            for (var i = 0; i < parameterSignatures.Length; ++i)
+            {
+                body.Emit(OpCodes.Ldarg, parameterSignatures[i].Position + 1);
+            }
+
+            body.Emit(OpCodes.Callvirt, signature);
+        }
+
+        #endregion
+    }
+}
