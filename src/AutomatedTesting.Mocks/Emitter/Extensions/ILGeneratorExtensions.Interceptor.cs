7462752186954f6341f0632da2c69d5c85d3dce7
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-27 17:47:51 +0100

    added dynamic code generator for a conditional decoratee call

--- src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Interceptor.cs
+++ src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Interceptor.cs
@@ -23,14 +23,18 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
         #region Logic
 
         /// <summary>
-        /// Emits the following source code:
-        /// <![CDATA[
-        ///     _interceptor.Intercept(invocation);
-        /// ]]>
+        /// Emits a call to the <see cref="IInterceptor.Intercept(IInvocation)"/> method.
         /// </summary>
         /// <param name="body"> The body of the dynamic method or property. </param>
         /// <param name="interceptorField"> The <see cref="IInterceptor"/> backing field. </param>
         /// <param name="invocationVariable"> The local <see cref="IInvocation"/> variable. </param>
+        /// <remarks>
+        /// Emits the following source code:
+        ///
+        /// <![CDATA[
+        ///     _interceptor.Intercept(invocation);
+        /// ]]>
+        /// </remarks>
         public static void EmitInterceptCall(
             this ILGenerator body, FieldBuilder interceptorField, LocalBuilder invocationVariable)
         {
@@ -39,6 +43,45 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
             body.Emit(OpCodes.Ldloc, invocationVariable.LocalIndex);
             body.Emit(OpCodes.Callvirt, Intercept.Value);
             body.Emit(OpCodes.Pop);
+        }
+
+        /// <summary>
+        /// Emits a call to the <see cref="IInterceptor.Intercept(IInvocation)"/> method along with an if statement.
+        /// </summary>
+        /// <param name="body"> The body of the dynamic method or property. </param>
+        /// <param name="interceptorField"> The <see cref="IInterceptor"/> backing field. </param>
+        /// <param name="invocationVariable"> The local <see cref="IInvocation"/> variable. </param>
+        /// <param name="elseLabel"> The label that defines the start of the "else" block. </param>
+        /// <remarks>
+        /// Emits the following source code:
+        /// 
+        /// <![CDATA[
+        ///     if(_interceptor.Intercept(invocation))
+        /// ]]>
+        /// </remarks>
+        public static void EmitIfInterceptCall(
+            this ILGenerator body,
+            FieldBuilder interceptorField,
+            LocalBuilder invocationVariable,
+            out Label elseLabel)
+        {
+            body.Emit(OpCodes.Ldarg_0);
+            body.Emit(OpCodes.Ldfld, interceptorField);
+            body.Emit(OpCodes.Ldloc, invocationVariable.LocalIndex);
+            body.Emit(OpCodes.Callvirt, Intercept.Value);
+            elseLabel = body.DefineLabel();
+            body.Emit(OpCodes.Brtrue_S, elseLabel);
+        }
+
+        /// <summary>
+        /// Emits an IL label for the matching <see cref="EmitIfInterceptCall(ILGenerator, FieldBuilder, LocalBuilder, out Label)"/>
+        /// call (i.e. the begin of the "else" block)
+        /// </summary>
+        /// <param name="body"> The body of the dynamic method or property. </param>
+        /// <param name="elseLabel"> The label that defines the start of the "else" block. </param>
+        public static void EmitElseInterceptCall(this ILGenerator body, Label elseLabel)
+        {
+            body.MarkLabel(elseLabel);
         }
 
         /// <summary>
