faf08a2f5b8d673e5e43cd4a963ccb0af9841e6a
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-27 18:00:22 +0100

    added unit tests for the DecorateActionEmitter type

--- src/Test.AutomatedTesting.Mocks/TestDomain/ActionDecorator/FooActionReferenceTypeParameterOut.cs
+++ src/Test.AutomatedTesting.Mocks/TestDomain/ActionDecorator/FooActionReferenceTypeParameterOut.cs
@@ -1,19 +1,19 @@
 namespace CustomCode.AutomatedTesting.Mocks.TestDomain
 {
     /// <summary>
-    /// Test domain implementation of the <see cref="IFooActionValueTypeParameterOut{T}"/> interface.
+    /// Test domain implementation of the <see cref="IFooActionReferenceTypeParameterOut{T}"/> interface.
     /// </summary>
     /// <typeparam name="T"> The value type of the method's parameters. </typeparam>
-    public sealed class FooActionValueTypeParameterOut<T> : IFooActionValueTypeParameterOut<T>
-        where T : struct
+    public sealed class FooActionReferenceTypeParameterOut<T> : IFooActionReferenceTypeParameterOut<T>
+        where T : class
     {
         #region Dependencies
 
         /// <summary>
-        /// Creates a new instance of the <see cref="FooActionValueTypeParameterOut{T}"/> type.
+        /// Creates a new instance of the <see cref="FooActionReferenceTypeParameterOut{T}"/> type.
         /// </summary>
         /// <param name="value"> The replaced value of the method's out parameter. </param>
-        public FooActionValueTypeParameterOut(T value)
+        public FooActionReferenceTypeParameterOut(T? value)
         {
             Value = value;
         }
@@ -23,21 +23,21 @@ namespace CustomCode.AutomatedTesting.Mocks.TestDomain
         #region Data
 
         /// <summary>
-        /// Gets the number of times the <see cref="MethodWithOneParameter(out T)"/> was called.
+        /// Gets the number of times the <see cref="MethodWithOneParameter(out T?)"/> was called.
         /// </summary>
         public uint CallCount { get; private set; } = 0;
 
         /// <summary>
         /// Gets the replaced value of the method's out parameter.
         /// </summary>
-        private T Value { get; }
+        private T? Value { get; }
 
         #endregion
 
         #region Logic
 
         /// <inheritdoc />
-        public void MethodWithOneParameter(out T first)
+        public void MethodWithOneParameter(out T? first)
         {
             CallCount++;
             first = Value;
