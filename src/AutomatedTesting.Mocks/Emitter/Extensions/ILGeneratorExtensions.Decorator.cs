2f2e8bb0c9453402ab0ef041d5d792d66719cdf9
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-29 21:33:18 +0200

    added dynamic code for calling decorated property setters

--- src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Decorator.cs
+++ src/AutomatedTesting.Mocks/Emitter/Extensions/ILGeneratorExtensions.Decorator.cs
@@ -16,6 +16,7 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
         /// <param name="signature"> The signature of the method to be called on the decorated instance. </param>
         /// <param name="parameterSignatures"> The signatures of the method's parameters. </param>
         /// <param name="decorateeField"> The backing field that holds the decorated instance. </param>
+        /// <param name="passSetterValue"> True to pass the value of a property setter, false otherwise. </param>
         /// <remarks>
         /// Emits the following source code:
         ///
@@ -27,7 +28,8 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
             this ILGenerator body,
             MethodInfo signature,
             ParameterInfo[] parameterSignatures,
-            FieldBuilder decorateeField)
+            FieldBuilder decorateeField,
+            bool passSetterValue = false)
         {
             body.Emit(OpCodes.Ldarg_0);
             body.Emit(OpCodes.Ldfld, decorateeField);
@@ -35,6 +37,11 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Extensions
             for (var i = 0; i < parameterSignatures.Length; ++i)
             {
                 body.Emit(OpCodes.Ldarg, parameterSignatures[i].Position + 1);
+            }
+
+            if (passSetterValue)
+            {
+                body.Emit(OpCodes.Ldarg, parameterSignatures.Length + 1);
             }
 
             body.Emit(OpCodes.Callvirt, signature);
