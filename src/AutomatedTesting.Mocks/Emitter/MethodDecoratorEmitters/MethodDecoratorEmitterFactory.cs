582d6d047ada4d17f99b86503d5627383b8561fd
Stefan Loeffler <s.loeffler@custom-co.de>
2020-03-31 15:31:50 +0200

    added functionality for DecorateAsyncMethodEmitter

--- src/AutomatedTesting.Mocks/Emitter/MethodDecoratorEmitters/MethodDecoratorEmitterFactory.cs
+++ src/AutomatedTesting.Mocks/Emitter/MethodDecoratorEmitters/MethodDecoratorEmitterFactory.cs
@@ -1,11 +1,14 @@
 namespace CustomCode.AutomatedTesting.Mocks.Emitter
 {
-    using CustomCode.AutomatedTesting.Mocks.ExceptionHandling;
+    using ExceptionHandling;
     using Interception;
+    using Interception.Async;
     using System;
     using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Reflection;
     using System.Reflection.Emit;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// Implementation of the <see cref="IMethodDecoratorEmitterFactory"/> interface for
@@ -14,6 +17,13 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter
     public sealed class MethodDecoratorEmitterFactory : IMethodDecoratorEmitterFactory
     {
         #region Data
+
+        /// <summary>
+        /// Gets a thread-safe cache that is used to store factories for creating strongly typed
+        /// <see cref="IMethodEmitter"/> instances for asynchronous methods.
+        /// </summary>
+        private static ConcurrentDictionary<Type, MethodDecoratorEmitterDelegate> AsyncMethodEmitterCache { get; }
+            = new ConcurrentDictionary<Type, MethodDecoratorEmitterDelegate>();
 
         /// <summary>
         /// Gets a thread-safe cache that is used to store factories for creating strongly typed
@@ -38,12 +48,98 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter
                 return new DecorateActionEmitter(type, signature, decoratee, interceptor);
             }
 
+            if (signature.ReturnType == typeof(Task))
+            {
+                return new DecorateAsyncMethodEmitter<AsyncTaskInvocation>(type, signature, decoratee, interceptor);
+            }
+
+            if (signature.ReturnType == typeof(ValueTask))
+            {
+                return new DecorateAsyncMethodEmitter<AsyncValueTaskInvocation>(type, signature, decoratee, interceptor);
+            }
+
+            if (signature.ReturnType.IsGenericType)
+            {
+                var returnTypeSignature = signature.ReturnType.GetGenericTypeDefinition();
+                if (returnTypeSignature == typeof(Task<>))
+                {
+                    var factory = AsyncMethodEmitterCache.GetOrAdd(
+                        signature.ReturnType,
+                        CreateAsyncMethodEmitterFactoryFor(signature.ReturnType, typeof(AsyncGenericTaskInvocation<>)));
+                    return factory(type, signature, decoratee, interceptor);
+                }
+
+                if (returnTypeSignature == typeof(ValueTask<>))
+                {
+                    var factory = AsyncMethodEmitterCache.GetOrAdd(
+                        signature.ReturnType,
+                        CreateAsyncMethodEmitterFactoryFor(signature.ReturnType, typeof(AsyncGenericValueTaskInvocation<>)));
+                    return factory(type, signature, decoratee, interceptor);
+                }
+
+                if (returnTypeSignature == typeof(IAsyncEnumerable<>))
+                {
+                    var factory = AsyncMethodEmitterCache.GetOrAdd(
+                        signature.ReturnType,
+                        CreateAsyncMethodEmitterFactoryFor(signature.ReturnType, typeof(AsyncIEnumerableInvocation<>)));
+                    return factory(type, signature, decoratee, interceptor);
+                }
+            }
+
             {
                 var factory = DecorateFuncEmitterCache.GetOrAdd(
                     signature.ReturnType,
                     CreateDecorateFuncEmitterFactoryFor(signature.ReturnType));
                 return factory(type, signature, decoratee, interceptor);
             }
+        }
+
+        /// <summary>
+        /// Use a <see cref="DynamicMethod"/> to create a factory delegate for a strongly typed
+        /// <see cref="DecorateAsyncMethodEmitter{T}" />.
+        /// </summary>
+        /// <param name="asyncType">
+        /// The return type of the asynchronous method (either <see cref="Task{TResult}"/>, <see cref="ValueTask{TResult}"/>
+        /// or <see cref="IAsyncEnumerable{T}"/>).
+        /// </param>
+        /// <param name="featureType">
+        /// The <see cref="IAsyncInvocation{T}"/> that is created by the emitter for each intercepted method invocation.
+        /// </param>
+        /// <returns> The created factory delegate. </returns>
+        /// <remarks>
+        /// Emits the following source code:
+        /// <![CDATA[
+        ///     IMethodEmitter Create(TypeBuilder type, MethodInfo signature, FieldBuilder decoratee, FieldBuilder interceptor)
+        ///     {
+        ///         return new DecorateAsyncMethodEmitter<FeatureType<AsyncType>>(type, signature, decoratee, interceptor);
+        ///     }
+        /// ]]>
+        /// </remarks>
+        private MethodDecoratorEmitterDelegate CreateAsyncMethodEmitterFactoryFor(Type asyncType, Type featureType)
+        {
+            var genericFeatureType = featureType.MakeGenericType(asyncType.GetGenericArguments());
+            var emitterType = typeof(DecorateAsyncMethodEmitter<>);
+            var genericEmitterType = emitterType.MakeGenericType(genericFeatureType);
+            var ctor = genericEmitterType
+                .GetConstructor(new[] { typeof(TypeBuilder), typeof(MethodInfo), typeof(FieldBuilder), typeof(FieldBuilder) })
+                ?? throw new ConstructorInfoException(genericEmitterType);
+
+            var dynamicFactory = new DynamicMethod(
+                $"Create{genericEmitterType.Name}",
+                typeof(IMethodEmitter),
+                new[] { typeof(TypeBuilder), typeof(MethodInfo), typeof(FieldBuilder), typeof(FieldBuilder) },
+                genericEmitterType);
+            var body = dynamicFactory.GetILGenerator();
+
+            body.Emit(OpCodes.Ldarg_0);
+            body.Emit(OpCodes.Ldarg_1);
+            body.Emit(OpCodes.Ldarg_2);
+            body.Emit(OpCodes.Ldarg_3);
+            body.Emit(OpCodes.Newobj, ctor);
+            body.Emit(OpCodes.Ret);
+
+            var factory = (MethodDecoratorEmitterDelegate)dynamicFactory.CreateDelegate(typeof(MethodDecoratorEmitterDelegate));
+            return factory;
         }
 
         /// <summary>
