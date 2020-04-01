512488eda1be2cdc9dcce054e0c26996a3ea9129
Stefan Loeffler <s.loeffler@custom-co.de>
2020-04-01 21:34:23 +0200

    added test domain for AsyncEnumerable decorators

---
+++ src/Test.AutomatedTesting.Mocks/TestDomain/AsyncDecorator/Enumerable/FooAsyncEnumerableReferenceTypeParameter.cs
@@ -0,0 +1,55 @@
+namespace CustomCode.AutomatedTesting.Mocks.TestDomain
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Test domain implementation of the <see cref="IFooAsyncEnumerableReferenceTypeParameter"/> interface.
+    /// </summary>
+    public sealed class FooAsyncEnumerableReferenceTypeParameter : IFooAsyncEnumerableReferenceTypeParameter
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FooAsyncEnumerableReferenceTypeParameter"/> type.
+        /// </summary>
+        /// <param name="result"> The method's return value. </param>
+        public FooAsyncEnumerableReferenceTypeParameter(IEnumerable<object?> result)
+        {
+            Result = result.ToAsyncEnumerable();
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// Gets the number of times the <see cref="MethodWithOneParameterAsync(object?)"/> was called.
+        /// </summary>
+        public uint CallCount { get; private set; } = 0;
+
+        /// <summary>
+        /// Gets the method's parameter.
+        /// </summary>
+        public object? Parameter { get; private set; }
+
+        /// <summary>
+        /// Gets the method's return value.
+        /// </summary>
+        private IAsyncEnumerable<object?> Result { get; }
+
+        #endregion
+
+        #region Logic
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<object?> MethodWithOneParameterAsync(object? first)
+        {
+            CallCount++;
+            Parameter = first;
+            return Result;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Test.AutomatedTesting.Mocks/TestDomain/AsyncDecorator/Enumerable/FooAsyncEnumerableValueTypeParameterless.cs
@@ -0,0 +1,49 @@
+namespace CustomCode.AutomatedTesting.Mocks.TestDomain
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Test domain implementation of the <see cref="IFooAsyncEnumerableValueTypeParameterless"/> interface.
+    /// </summary>
+    public sealed class FooAsyncEnumerableValueTypeParameterless : IFooAsyncEnumerableValueTypeParameterless
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FooAsyncEnumerableValueTypeParameterless"/> type.
+        /// </summary>
+        /// <param name="result"> The method's return value. </param>
+        public FooAsyncEnumerableValueTypeParameterless(IEnumerable<int> result)
+        {
+            Result = result.ToAsyncEnumerable();
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// Gets the number of times the <see cref="MethodWithoutParameterAsync"/> was called.
+        /// </summary>
+        public uint CallCount { get; private set; } = 0;
+
+        /// <summary>
+        /// Gets the method's return value.
+        /// </summary>
+        private IAsyncEnumerable<int> Result { get; }
+
+        #endregion
+
+        #region Logic
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<int> MethodWithoutParameterAsync()
+        {
+            CallCount++;
+            return Result;
+        }
+
+        #endregion
+    }
+}
