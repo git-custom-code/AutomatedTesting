561a732aa63550a82aa01f03767b00ec44d682a7
Stefan Loeffler <s.loeffler@custom-co.de>
2020-04-03 19:04:51 +0200

    added custom exception handling

---
+++ src/AutomatedTesting.Analyzer/ExceptionHandling/CompilationErrorsException.cs
@@ -0,0 +1,35 @@
+namespace CustomCode.AutomatedTesting.Analyzer.ExceptionHandling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Exception that is thrown when an <see cref="InMemoryProject"/> was compiled with errors.
+    /// </summary>
+    public sealed class CompilationErrorsException : Exception
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="CompilationErrorsException"/> type.
+        /// </summary>
+        /// <param name="errors"> The compilation errors. </param>
+        public CompilationErrorsException(IEnumerable<string> errors)
+            : base($"In-memory project has {errors.Count()} compilation errors")
+        {
+            Errors = errors;
+        }
+
+        #endregion
+
+        #region Data
+
+        /// <summary>
+        /// Gets the compilation errors.
+        /// </summary>
+        public IEnumerable<string> Errors { get; }
+
+        #endregion
+    }
+}
---
+++ src/AutomatedTesting.Analyzer/ExceptionHandling/CompilationException.cs
@@ -0,0 +1,31 @@
+namespace CustomCode.AutomatedTesting.Analyzer.ExceptionHandling
+{
+    using System;
+
+    /// <summary>
+    /// Exception that is thrown when an <see cref="InMemoryProject"/> could not be compiled.
+    /// </summary>
+    public sealed class CompilationException : Exception
+    {
+        #region Dependencies
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="CompilationException"/> type.
+        /// </summary>
+        /// <param name="message"> The exception's error message. </param>
+        public CompilationException(string message)
+            : base (message)
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="CompilationException"/> type.
+        /// </summary>
+        /// <param name="message"> The exception's error message. </param>
+        /// <param name="innerException"> The causing exception. </param>
+        public CompilationException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        #endregion
+    }
+}
