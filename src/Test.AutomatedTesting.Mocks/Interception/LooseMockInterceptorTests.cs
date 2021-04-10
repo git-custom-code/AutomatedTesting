81feb8716202596e414ffe42428e77677fb0e3a7
Stefan Loeffler <s.loeffler@custom-co.de>
2021-04-10 14:59:02 +0200

    added missing unit tests for LooseMockInterceptor and PartialMockInterceptor

---
+++ src/Test.AutomatedTesting.Mocks/Interception/LooseMockInterceptorTests.cs
@@ -0,0 +1,58 @@
+namespace CustomCode.AutomatedTesting.Mocks.Interception.Tests
+{
+    using Arrangements;
+    using ExceptionHandling;
+    using ReturnValue;
+    using Xunit;
+
+    /// <summary>
+    /// Automated tests for the <see cref="LooseMockInterceptor"/> type.
+    /// </summary>
+    public sealed class LooseMockInterceptorTests
+    {
+        [Fact(DisplayName = "MockBehavior.Loose: successfull invocation with arrangement")]
+        public void SuccessfullInvocationWithArrangement()
+        {
+            // Given
+            var type = typeof(object);
+            var methodName = nameof(object.GetHashCode);
+            var signature = type.GetMethod(methodName) ?? throw new MethodInfoException(type, methodName);
+            var methodInvocation = new Invocation(signature, new ReturnValueInvocation<int>());
+
+            var arrangement = new ReturnValueArrangement<int>(signature, 42);
+            var arrangements = new ArrangementCollection(new IArrangement[] { arrangement });
+            var interceptor = new LooseMockInterceptor(arrangements);
+
+            // When
+            var wasIntercepted = interceptor.Intercept(methodInvocation);
+
+            // Then
+            Assert.True(wasIntercepted);
+            var hasFeature = methodInvocation.TryGetFeature<IReturnValue<int>>(out var feature);
+            Assert.True(hasFeature);
+            Assert.Equal(42, feature?.ReturnValue);
+        }
+
+        [Fact(DisplayName = "MockBehavior.Loose: successfull invocation without arrangement")]
+        public void SuccessfullInvocationWithoutArrangement()
+        {
+            // Given
+            var type = typeof(object);
+            var methodName = nameof(object.GetHashCode);
+            var signature = type.GetMethod(methodName) ?? throw new MethodInfoException(type, methodName);
+            var methodInvocation = new Invocation(signature, new ReturnValueInvocation<int>());
+
+            var emptyArrangements = new ArrangementCollection();
+            var interceptor = new LooseMockInterceptor(emptyArrangements);
+
+            // When
+            var wasIntercepted = interceptor.Intercept(methodInvocation);
+
+            // Then
+            Assert.True(wasIntercepted);
+            var hasFeature = methodInvocation.TryGetFeature<IReturnValue<int>>(out var feature);
+            Assert.True(hasFeature);
+            Assert.Equal(default(int), feature?.ReturnValue);
+        }
+    }
+}
