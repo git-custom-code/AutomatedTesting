8e8f1dd6b5c95ac7e9122b58cde804e48dd47d48
Stefan Loeffler <s.loeffler@custom-co.de>
2021-04-10 14:04:07 +0200

    fixed unit tests after additional null checks

--- src/Test.AutomatedTesting.Mocks/Emitter/PropertyEmitters/PropertyEmitterFactoryTests.cs
+++ src/Test.AutomatedTesting.Mocks/Emitter/PropertyEmitters/PropertyEmitterFactoryTests.cs
@@ -1,6 +1,8 @@
 namespace CustomCode.AutomatedTesting.Mocks.Emitter.Tests
 {
+    using CustomCode.AutomatedTesting.Mocks.Interception;
     using ExceptionHandling;
+    using System.Reflection;
     using System.Reflection.Emit;
     using TestDomain;
     using Xunit;
@@ -14,17 +16,19 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Tests
         public void CreateEmitterForPropertyGetter()
         {
             // Given
-            TypeBuilder? typeBuilder = null;
-            FieldBuilder? interceptor = null;
+            var name = new AssemblyName("DynamicMockAssembly");
+            var assembly = AssemblyBuilder.DefineDynamicAssembly(name, AssemblyBuilderAccess.RunAndCollect);
+            var module = assembly.DefineDynamicModule("DynamicMockModule");
+
+            var typeBuilder = module.DefineType("DynamicType");
+            var interceptor = typeBuilder.DefineField("_interceptor", typeof(IInterceptor), FieldAttributes.Private);
             var type = typeof(IFooValueTypeGetter<int>);
             var propertyName = nameof(IFooValueTypeGetter<int>.Getter);
             var property = type.GetProperty(propertyName) ?? throw new PropertyInfoException(type, propertyName);
             var factory = new PropertyEmitterFactory();
 
             // When
-#nullable disable
             var emitter = factory.CreatePropertyEmitterFor(property, typeBuilder, interceptor);
-#nullable restore
 
             // Then
             Assert.NotNull(emitter);
@@ -35,17 +39,19 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Tests
         public void CreateEmitterForPropertySetter()
         {
             // Given
-            TypeBuilder? typeBuilder = null;
-            FieldBuilder? interceptor = null;
+            var name = new AssemblyName("DynamicMockAssembly");
+            var assembly = AssemblyBuilder.DefineDynamicAssembly(name, AssemblyBuilderAccess.RunAndCollect);
+            var module = assembly.DefineDynamicModule("DynamicMockModule");
+
+            var typeBuilder = module.DefineType("DynamicType");
+            var interceptor = typeBuilder.DefineField("_interceptor", typeof(IInterceptor), FieldAttributes.Private);
             var type = typeof(IFooValueTypeSetter<int>);
             var propertyName = nameof(IFooValueTypeSetter<int>.Setter);
             var property = type.GetProperty(propertyName) ?? throw new PropertyInfoException(type, propertyName);
             var factory = new PropertyEmitterFactory();
 
             // When
-#nullable disable
             var emitter = factory.CreatePropertyEmitterFor(property, typeBuilder, interceptor);
-#nullable restore
 
             // Then
             Assert.NotNull(emitter);
@@ -56,17 +62,19 @@ namespace CustomCode.AutomatedTesting.Mocks.Emitter.Tests
         public void CreateEmitterForPropertyGetterSetter()
         {
             // Given
-            TypeBuilder? typeBuilder = null;
-            FieldBuilder? interceptor = null;
+            var name = new AssemblyName("DynamicMockAssembly");
+            var assembly = AssemblyBuilder.DefineDynamicAssembly(name, AssemblyBuilderAccess.RunAndCollect);
+            var module = assembly.DefineDynamicModule("DynamicMockModule");
+
+            var typeBuilder = module.DefineType("DynamicType");
+            var interceptor = typeBuilder.DefineField("_interceptor", typeof(IInterceptor), FieldAttributes.Private);
             var type = typeof(IFooValueTypeProperty<int>);
             var propertyName = nameof(IFooValueTypeProperty<int>.GetterSetter);
             var property = type.GetProperty(propertyName) ?? throw new PropertyInfoException(type, propertyName);
             var factory = new PropertyEmitterFactory();
 
             // When
-#nullable disable
             var emitter = factory.CreatePropertyEmitterFor(property, typeBuilder, interceptor);
-#nullable restore
 
             // Then
             Assert.NotNull(emitter);
