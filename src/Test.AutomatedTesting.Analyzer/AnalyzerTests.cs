338825c2d410b67e5d360f16e744fd718d63690c
Stefan Loeffler <s.loeffler@custom-co.de>
2022-07-14 17:12:32 +0200

    updated to file-scoped namespaces

--- src/Test.AutomatedTesting.Analyzer/AnalyzerTests.cs
+++ src/Test.AutomatedTesting.Analyzer/AnalyzerTests.cs
@@ -1,23 +1,22 @@
-namespace CustomCode.AutomatedTesting.Analyzer.Tests
+namespace CustomCode.AutomatedTesting.Analyzer.Tests;
+
+using TestDomain;
+using Xunit;
+
+/// <summary>
+/// Automated tests for the <see cref="Analyzer"/> type.
+/// </summary>
+public sealed class AnalyzerTests
 {
-    using TestDomain;
-    using Xunit;
-
-    /// <summary>
-    /// Automated tests for the <see cref="Analyzer"/> type.
-    /// </summary>
-    public sealed class AnalyzerTests
+    [Fact(DisplayName = "Analyzer: create mocked analyzer")]
+    public void CreateMocked()
     {
-        [Fact(DisplayName = "Analyzer: create mocked analyzer")]
-        public void CreateMocked()
-        {
-            // Given
+        // Given
 
-            // When
-            var mocked = Analyzer.CreateMocked<FooAnalyzer>();
+        // When
+        var mocked = Analyzer.CreateMocked<FooAnalyzer>();
 
-            // Then
-            Assert.NotNull(mocked);
-        }
+        // Then
+        Assert.NotNull(mocked);
     }
 }
--- src/Test.AutomatedTesting.Analyzer/Core/InMemoryProjectTests.cs
+++ src/Test.AutomatedTesting.Analyzer/Core/InMemoryProjectTests.cs
@@ -1,87 +1,86 @@
-namespace CustomCode.AutomatedTesting.Analyzer.Core.Tests
+namespace CustomCode.AutomatedTesting.Analyzer.Core.Tests;
+
+using System.Threading.Tasks;
+using Xunit;
+
+/// <summary>
+/// Automated tests for the <see cref="InMemoryProject"/> type.
+/// </summary>
+public sealed class InMemoryProjectTests
 {
-    using System.Threading.Tasks;
-    using Xunit;
-
-    /// <summary>
-    /// Automated tests for the <see cref="InMemoryProject"/> type.
-    /// </summary>
-    public sealed class InMemoryProjectTests
+    [Fact(DisplayName = "InMemoryProject: add class")]
+    public async Task AddClassAsync()
     {
-        [Fact(DisplayName = "InMemoryProject: add class")]
-        public async Task AddClassAsync()
-        {
-            // Given
-            var project = new InMemoryProject();
+        // Given
+        var project = new InMemoryProject();
 
-            // When
-            project.Add("public class Foo { }");
-            var compilation = await project.CompileAsync();
+        // When
+        project.Add("public class Foo { }");
+        var compilation = await project.CompileAsync();
 
-            // Then
-            Assert.NotNull(compilation);
-            Assert.True(compilation.ContainsSymbolsWithName("Foo"));
-        }
+        // Then
+        Assert.NotNull(compilation);
+        Assert.True(compilation.ContainsSymbolsWithName("Foo"));
+    }
 
-        [Fact(DisplayName = "InMemoryProject: add or update class")]
-        public async Task AddOrUpdateClassAsync()
-        {
-            // Given
-            var project = new InMemoryProject();
+    [Fact(DisplayName = "InMemoryProject: add or update class")]
+    public async Task AddOrUpdateClassAsync()
+    {
+        // Given
+        var project = new InMemoryProject();
 
-            // When
-            project.AddOrUpdate("Foo.cs", "public class Foo { }");
-            var compilation = await project.CompileAsync();
+        // When
+        project.AddOrUpdate("Foo.cs", "public class Foo { }");
+        var compilation = await project.CompileAsync();
 
-            // Then
-            Assert.NotNull(compilation);
-            Assert.True(compilation.ContainsSymbolsWithName("Foo"));
-        }
+        // Then
+        Assert.NotNull(compilation);
+        Assert.True(compilation.ContainsSymbolsWithName("Foo"));
+    }
 
-        [Fact(DisplayName = "InMemoryProject: add or update source code")]
-        public async Task AddOrUpdateSourceCodeAsync()
-        {
-            // Given
-            var project = new InMemoryProject();
-            var code = new SourceCodeFile("Foo.cs", "public class Foo { }");
+    [Fact(DisplayName = "InMemoryProject: add or update source code")]
+    public async Task AddOrUpdateSourceCodeAsync()
+    {
+        // Given
+        var project = new InMemoryProject();
+        var code = new SourceCodeFile("Foo.cs", "public class Foo { }");
 
-            // When
-            project.AddOrUpdate(code);
-            var compilation = await project.CompileAsync();
+        // When
+        project.AddOrUpdate(code);
+        var compilation = await project.CompileAsync();
 
-            // Then
-            Assert.NotNull(compilation);
-            Assert.True(compilation.ContainsSymbolsWithName("Foo"));
-        }
+        // Then
+        Assert.NotNull(compilation);
+        Assert.True(compilation.ContainsSymbolsWithName("Foo"));
+    }
 
-        [Fact(DisplayName = "InMemoryProject: add reference")]
-        public async Task AddReferenceAsync()
-        {
-            // Given
-            var project = new InMemoryProject();
+    [Fact(DisplayName = "InMemoryProject: add reference")]
+    public async Task AddReferenceAsync()
+    {
+        // Given
+        var project = new InMemoryProject();
 
-            // When
-            project.AddReference(typeof(InMemoryProject).Assembly);
-            var compilation = await project.CompileAsync();
+        // When
+        project.AddReference(typeof(InMemoryProject).Assembly);
+        var compilation = await project.CompileAsync();
 
-            // Then
-            Assert.NotNull(compilation);
-            Assert.Contains(compilation.ReferencedAssemblyNames, a => a.Name == typeof(InMemoryProject).Assembly.GetName().Name);
-        }
+        // Then
+        Assert.NotNull(compilation);
+        Assert.Contains(compilation.ReferencedAssemblyNames, a => a.Name == typeof(InMemoryProject).Assembly.GetName().Name);
+    }
 
-        [Fact(DisplayName = "InMemoryProject: add reference for type")]
-        public async Task AddReferenceForTypeAsync()
-        {
-            // Given
-            var project = new InMemoryProject();
+    [Fact(DisplayName = "InMemoryProject: add reference for type")]
+    public async Task AddReferenceForTypeAsync()
+    {
+        // Given
+        var project = new InMemoryProject();
 
-            // When
-            project.AddReferenceFor<InMemoryProject>();
-            var compilation = await project.CompileAsync();
+        // When
+        project.AddReferenceFor<InMemoryProject>();
+        var compilation = await project.CompileAsync();
 
-            // Then
-            Assert.NotNull(compilation);
-            Assert.Contains(compilation.ReferencedAssemblyNames, a => a.Name == typeof(InMemoryProject).Assembly.GetName().Name);
-        }
+        // Then
+        Assert.NotNull(compilation);
+        Assert.Contains(compilation.ReferencedAssemblyNames, a => a.Name == typeof(InMemoryProject).Assembly.GetName().Name);
     }
 }
