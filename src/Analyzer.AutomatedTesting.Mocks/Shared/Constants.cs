c3c03d5c5753cae412309b13fce11142108b72b1
Stefan Loeffler <s.loeffler@custom-co.de>
2022-07-14 16:14:06 +0200

    updated to file-scoped namespaces

--- src/Analyzer.AutomatedTesting.Mocks/Shared/Constants.cs
+++ src/Analyzer.AutomatedTesting.Mocks/Shared/Constants.cs
@@ -1,32 +1,31 @@
-namespace CustomCode.Analyzer.AutomatedTesting.Mocks.Shared
+namespace CustomCode.Analyzer.AutomatedTesting.Mocks.Shared;
+
+/// <summary>
+/// Type that contains shared string constants.
+/// </summary>
+public sealed class Constants
 {
+    #region Data
+
     /// <summary>
-    /// Type that contains shared string constants.
+    /// The name of the "Mock" type.
     /// </summary>
-    public sealed class Constants
-    {
-        #region Data
+    public const string TypeNameMock = "Mock";
 
-        /// <summary>
-        /// The name of the "Mock" type.
-        /// </summary>
-        public const string TypeNameMock = "Mock";
+    /// <summary>
+    /// The name of the "Mock.CreateMocked" method.
+    /// </summary>
+    public const string MethodNameCreateMocked = "CreateMocked";
 
-        /// <summary>
-        /// The name of the "Mock.CreateMocked" method.
-        /// </summary>
-        public const string MethodNameCreateMocked = "CreateMocked";
+    /// <summary>
+    /// The name of the "CustomCode.AutomatedTesting.Mocks" namespace.
+    /// </summary>
+    public const string NamespaceMocks = "CustomCode.AutomatedTesting.Mocks";
 
-        /// <summary>
-        /// The name of the "CustomCode.AutomatedTesting.Mocks" namespace.
-        /// </summary>
-        public const string NamespaceMocks = "CustomCode.AutomatedTesting.Mocks";
+    /// <summary>
+    /// The shared category that is used by all analyzers.
+    /// </summary>
+    public const string Category = "AutomatedTesting.Mocks";
 
-        /// <summary>
-        /// The shared category that is used by all analyzers.
-        /// </summary>
-        public const string Category = "AutomatedTesting.Mocks";
-
-        #endregion
-    }
+    #endregion
 }
